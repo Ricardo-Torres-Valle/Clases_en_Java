40d74f561d84ae7005a35e2dd389d3fe2bcf0eb9
Rick <Rick@DESKTOP-N10MD3R>
2022-03-08 14:49:28 -0600

    49. Objetos en Java -continuacion

--- src/clases/PruebaPersona.java
+++ src/clases/PruebaPersona.java
@@ -4,11 +4,17 @@ package clases;
 public class PruebaPersona {
     
     public static void main(String[] args) {
-        Persona persona1;
-        persona1 = new Persona();
+        Persona persona1 = new Persona();
         persona1.nombre = "Juan";
         persona1.apellido = "Perez";
         persona1.desplegarInformacion();
+        
+        Persona persona2 = new Persona();
+        
+        System.out.println("persona1 = "+persona1);
+        System.out.println("persona2 = "+persona2);
+        
+        persona2.desplegarInformacion();
     }
     
 }
